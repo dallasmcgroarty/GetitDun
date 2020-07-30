6454e83bd8b095cb21a044de6ab3405b0544d7cb
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-07-29 18:41:29 -0700

    viewing and editing todos

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
+from .models import Todo
 
 def home(request):
     return render(request,'home.html')
@@ -58,4 +59,18 @@ def createtodo(request):
             return render(request, 'createtodo.html', {'form': TodoForm(), 'error':'Bad data passed in. Try again.'})
 
 def currenttodos(request):
-    return render(request, 'currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'currenttodos.html', {'todos':todos})
+
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'viewtodo.html', {'todo':todo,'form':form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'viewtodo.html', {'todo':todo, 'form':form, 'error':'Bad info'})
--- todoproj/urls.py
+++ todoproj/urls.py
@@ -23,8 +23,10 @@ urlpatterns = [
     path('signup/', views.signupuser, name='signupuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
     path('login/', views.loginuser, name='loginuser'),
+
     # Todos
     path('', views.home, name='home'),
     path('create/', views.createtodo, name='createtodo'),
-    path('current/', views.currenttodos, name='currenttodos')
+    path('current/', views.currenttodos, name='currenttodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo')
 ]
